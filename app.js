4cc27215ba1b178a9ce2a23f6186cc35291a9766
Anthony Fattman <AFattman@1AAuto.com>
2018-10-27 04:08:44 -0400

    About, Get Involved and Donate Pages added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
+
+//basic routes sent to index.js and all other routing handled from there :)
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+//Initilaizes the app as a whole
 var app = express();
 
 // view engine setup
@@ -19,8 +24,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.post('/http://localhost:8090/contacts', function(req, res) {
+ var contactAry = { id: 2,
+                  name: 'Fattman',
+                  email: 'Dudes101@Gmail.com',
+                  message: 'Yo kevin, biggest fan ever'}
+});
+
+app.get('/', function(req, res){
+  res.send('Hey');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +53,78 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  
 });
+//middle ware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+app.get('contact/sendcontact/:email', function(req, res){
+  contact.sendcontact(req.params.id, function(err, contact){
+    res.render('contact_info', {
+      title: 'Contact',
+      contact:contact
+    });
+  });
+});
+
+
+
+
+//not using this just yet
+app.post('not sure yet', function(req, res){
+  let contact = new contact();
+  contact.name = req.body.name;
+  contact.subject = req.body.author;
+  contact.email = req.body.email;
+  contact.message = req.body.message;
+
+
+});
+
+app.post('/success', function(req, res){
+  let contact = new contact();
+  contact.name = req.body.name;
+  contact.subject = req.body.subject;
+  contact.email = req.body.email;
+  contact.message = req.body.message;
+});
+
+
+// add contact to db
+app.post('/contact', function(req, res){
+  let contact = new contact();
+
+  contact.name = req.body.name;
+  contact.subject = req.body.subject;
+  contact.email = req.body.email;
+  contact.message = req.body.message;
+
+  console.log('Submitted');
+  console.log(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else{
+      res.render('/success', {name: req.body.name, 
+                                subject: req.body.subject, 
+                                email: req.body.email, 
+                                message: req.body.message});
+    }
+  });
+
+
+  return;
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Fattman for Senate' });
 });
+
+// 
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Fattman for Senate' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Fattman for Senate' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Fattman for Senate' });
+});
+
+router.get('/getinvolved', function(req, res, next) {
+  res.render('getinvolved', { title: 'Fattman for Senate' });
+});
+
+router.get('/donate', function(req, res, next) {
+  res.render('donate', { title: 'Fattman for Senate' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Fattman for Senate' });
+});
+
+
+
 
 module.exports = router;
